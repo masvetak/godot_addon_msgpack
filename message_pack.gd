58b968e3549c32ec67061ea3e47f62cecd49ed69
masvetak <masvetak@gmail.com>
2023-06-29 11:22:32 +0200

    Update file

--- message_pack.gd
+++ message_pack.gd
@@ -5,15 +5,19 @@
 # @version     0.1.0
 # ------------------------------------------------------------------------------
 
-class_name Msgpck
-extends Object
+class_name Msgpck extends Object
+
+# ------------------------------------------------------------------------------
+# Public methods
+# ------------------------------------------------------------------------------
 
 static func encode(value):
-	var ctx = {error = OK, error_string = ""}
 	var buffer = StreamPeerBuffer.new()
 	buffer.big_endian = true
-
+	
+	var ctx = {error = OK, error_string = ""}
 	_encode(buffer, value, ctx)
+	
 	if ctx.error == OK:
 		return {
 			result = buffer.data_array,
@@ -31,9 +35,10 @@ static func decode(bytes):
 	var buffer = StreamPeerBuffer.new()
 	buffer.big_endian = true
 	buffer.data_array = bytes
-
+	
 	var ctx = {error = OK, error_string = ""}
 	var value = _decode(buffer, ctx)
+	
 	if ctx.error == OK:
 		if buffer.get_position() == buffer.get_size():
 			return {result = value, error = OK, error_string = ""}
@@ -43,17 +48,19 @@ static func decode(bytes):
 	else:
 		return {result = null, error = ctx.error, error_string = ctx.error_string}
 
+# ------------------------------------------------------------------------------
+# Private methods
+# ------------------------------------------------------------------------------
+
 static func _encode(buf, value, ctx):
 	match typeof(value):
 		TYPE_NIL:
 			buf.put_u8(0xc0)
-		
 		TYPE_BOOL:
 			if value:
 				buf.put_u8(0xc3)
 			else:
 				buf.put_u8(0xc2)
-		
 		TYPE_INT:
 			if -(1 << 5) <= value and value <= (1 << 7) - 1:
 				# fixnum (positive and negative)
@@ -70,14 +77,11 @@ static func _encode(buf, value, ctx):
 			else:
 				buf.put_u8(0xd3)
 				buf.put_64(value)
-		
 		TYPE_FLOAT:
 			buf.put_u8(0xca)
 			buf.put_float(value)
-		
 		TYPE_STRING:
 			var bytes = value.to_utf8_buffer()
-			
 			var size = bytes.size()
 			if size <= (1 << 5) - 1:
 				# type fixstr [101XXXXX]
@@ -97,9 +101,7 @@ static func _encode(buf, value, ctx):
 			else:
 				@warning_ignore("assert_always_false")
 				assert(false)
-			
 			buf.put_data(bytes)
-		
 		TYPE_PACKED_BYTE_ARRAY:
 			var size = value.size()
 			if size <= (1 << 8) - 1:
@@ -114,9 +116,7 @@ static func _encode(buf, value, ctx):
 			else:
 				@warning_ignore("assert_always_false")
 				assert(false)
-			
 			buf.put_data(value)
-
 		TYPE_ARRAY:
 			var size = value.size()
 			if size <= 15:
@@ -137,7 +137,6 @@ static func _encode(buf, value, ctx):
 				_encode(buf, obj, ctx)
 				if ctx.error != OK:
 					return
-
 		TYPE_DICTIONARY:
 			var size = value.size()
 			if size <= 15:
@@ -154,7 +153,6 @@ static func _encode(buf, value, ctx):
 			else:
 				@warning_ignore("assert_always_false")
 				assert(false)
-			
 			for key in value:
 				_encode(buf, key, ctx)
 				if ctx.error != OK:
@@ -172,7 +170,6 @@ static func _decode(buffer, ctx):
 		ctx.error = FAILED
 		ctx.error_string = "unexpected end of input"
 		return null
-
 	var head = buffer.get_u8()
 	if head == 0xc0:
 		return null
@@ -180,7 +177,6 @@ static func _decode(buffer, ctx):
 		return false
 	elif head == 0xc3:
 		return true
-
 	# Integers
 	elif head & 0x80 == 0:
 		# positive fixnum
@@ -194,7 +190,6 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for uint8"
 			return null
-
 		return buffer.get_u8()
 	elif head == 0xcd:
 		# uint 16
@@ -202,7 +197,6 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for uint16"
 			return null
-
 		return buffer.get_u16()
 	elif head == 0xce:
 		# uint 32
@@ -210,7 +204,6 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for uint32"
 			return null
-
 		return buffer.get_u32()
 	elif head == 0xcf:
 		# uint 64
@@ -218,7 +211,6 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for uint64"
 			return null
-
 		return buffer.get_u64()
 	elif head == 0xd0:
 		# int 8
@@ -226,7 +218,6 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enogh buffer for int8"
 			return null
-
 		return buffer.get_8()
 	elif head == 0xd1:
 		# int 16
@@ -234,7 +225,6 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enogh buffer for int16"
 			return null
-
 		return buffer.get_16()
 	elif head == 0xd2:
 		# int 32
@@ -242,7 +232,6 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for int32"
 			return null
-
 		return buffer.get_32()
 	elif head == 0xd3:
 		# int 64
@@ -250,9 +239,7 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for int64"
 			return null
-
 		return buffer.get_64()
-
 	# Float
 	elif head == 0xca:
 		# float32
@@ -260,7 +247,6 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for float32"
 			return null
-
 		return buffer.get_float()
 	elif head == 0xcb:
 		# float64
@@ -268,9 +254,7 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for float64"
 			return null
-
 		return buffer.get_double()
-
 	# String
 	elif (~head) & 0xa0 == 0:
 		var size = head & 0x1f
@@ -278,61 +262,51 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for fixstr required %s bytes" % [size]
 			return null
-
 		return buffer.get_utf8_string(size)
 	elif head == 0xd9:
 		if buffer.get_size() - buffer.get_position() < 1:
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for str8 size"
 			return null
-
 		var size = buffer.get_u8()
 		if buffer.get_size() - buffer.get_position() < size:
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for str8 data required %s bytes" % [size]
 			return null
-
 		return buffer.get_utf8_string(size)
 	elif head == 0xda:
 		if buffer.get_size() - buffer.get_position() < 2:
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for str16 size"
 			return null
-
 		var size = buffer.get_u16()
 		if buffer.get_size() - buffer.get_position() < size:
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for str16 data required %s bytes" % [size]
 			return null
-
 		return buffer.get_utf8_string(size)
 	elif head == 0xdb:
 		if buffer.get_size() - buffer.get_position() < 4:
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for str32 size"
 			return null
-
 		var size = buffer.get_u32()
 		if buffer.get_size() - buffer.get_position() < size:
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for str32 data required %s bytes" % [size]
 			return null
-
 		return buffer.get_utf8_string(size)
-
 	# Binary
 	elif head == 0xc4:
 		if buffer.get_size() - buffer.get_position() < 1:
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for bin8 size"
 			return null
-
 		var size = buffer.get_u8()
 		if buffer.get_size() - buffer.get_position() < size:
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for bin8 data required %s bytes" % [size]
 			return null
-
 		var res = buffer.get_data(size)
 		assert(res[0] == OK)
 		return res[1]
@@ -341,13 +315,11 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for bin16 size"
 			return null
-
 		var size = buffer.get_u16()
 		if buffer.get_size() - buffer.get_position() < size:
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for bin16 data required %s bytes" % [size]
 			return null
-
 		var res = buffer.get_data(size)
 		assert(res[0] == OK)
 		return res[1]
@@ -356,17 +328,14 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for bin32 size"
 			return null
-
 		var size = buffer.get_u32()
 		if buffer.get_size() - buffer.get_position() < size:
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for bin32 data required %s bytes" % [size]
 			return null
-
 		var res = buffer.get_data(size)
 		assert(res[0] == OK)
 		return res[1]
-
 	# Array
 	elif head & 0xf0 == 0x90:
 		var size = head & 0x0f
@@ -381,7 +350,6 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for array16 size"
 			return null
-
 		var size = buffer.get_u16()
 		var res = []
 		for _i in range(size):
@@ -394,7 +362,6 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for array32 size"
 			return null
-
 		var size = buffer.get_u32()
 		var res = []
 		for _i in range(size):
@@ -402,7 +369,6 @@ static func _decode(buffer, ctx):
 			if ctx.error != OK:
 				return null
 		return res
-
 	# Map
 	elif head & 0xf0 == 0x80:
 		var size = head & 0x0f
@@ -411,11 +377,9 @@ static func _decode(buffer, ctx):
 			var k = _decode(buffer, ctx)
 			if ctx.error != OK:
 				return null
-
 			var v = _decode(buffer, ctx)
 			if ctx.error != OK:
 				return null
-
 			res[k] = v
 		return res
 	elif head == 0xde:
@@ -423,18 +387,15 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for map16 size"
 			return null
-
 		var size = buffer.get_u16()
 		var res = {}
 		for _i in range(size):
 			var k = _decode(buffer, ctx)
 			if ctx.error != OK:
 				return null
-
 			var v = _decode(buffer, ctx)
 			if ctx.error != OK:
 				return null
-
 			res[k] = v
 		return res
 	elif head == 0xdf:
@@ -442,21 +403,17 @@ static func _decode(buffer, ctx):
 			ctx.error = FAILED
 			ctx.error_string = "not enough buffer for map32 size"
 			return null
-
 		var size = buffer.get_u32()
 		var res = {}
 		for _i in range(size):
 			var k = _decode(buffer, ctx)
 			if ctx.error != OK:
 				return null
-
 			var v = _decode(buffer, ctx)
 			if ctx.error != OK:
 				return null
-
 			res[k] = v
 		return res
-
 	else:
 		ctx.error = FAILED
 		ctx.error_string = "invalid byte tag %02X at pos %s" % [head, buffer.get_position()]
